6605ca6c31ae2e5171406d8f6415298769cf90e1
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-10-12 10:59:52 -0300

    Criação do projeto

---
+++ GestaoProdutos.API/Model/FornecedorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoProdutos.API.Model
+{
+    public class FornecedorViewModel
+    {
+    }
+}
